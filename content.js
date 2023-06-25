ac6a474f48770b80146ccccbc28c3e263ab2097b
tiberiu-zota <tiberiu.zota@protonmail.com>
2023-06-25 22:54:55 +0300

    Scrie Realizată

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(request.method == "changePage"){
-            t = document.body.innerText;
-            document.body.innerText = "Text de exemplu";
-            sendResponse({text: t, method: "changePage"}); //same as innerText
+            //t = document.body.innerText;
+            //document.body.innerText = "Text de exemplu";
+            //sendResponse({text: t, method: "changePage"}); //same as innerText
+            const collection = document.getElementsByClassName("ui-cell-editor-output");
+            for (let i = 0; i < collection.length; i++) {
+                collection[i].innerText = "Realizată";
+            }
         }
     }
 );

8840459b0dd313205505a3392c6d5bc243556643
tibi-herastrau <zota.tiberiu@scoala12bucuresti.ro>
2023-06-30 22:14:28 +0300

    Incercari shortcut manifest

--- popup.js
+++ popup.js
@@ -24,3 +24,10 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }, false);
 }, false);
+
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { method: "realizatUnu" }, function (response) {
+        
+    });
+});
